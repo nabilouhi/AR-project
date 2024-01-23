8425334641e3f2255ef8c8f316d2ef5a3d38f7be
nabilouhi <nabilouhi@hotmail.com>
2024-01-24 00:17:25 +0100

    Update script.js

--- v1/script.js
+++ v1/script.js
@@ -26,13 +26,13 @@ var models = [
     }
 ];
 
-var modelIndex = 0;
 var setModel = function (model, entity) {
     if (model.scale) { entity.setAttribute('scale', model.scale);}
     if (model.rotation) { entity.setAttribute('rotation', model.rotation);}
     if (model.position) { entity.setAttribute('position', model.position);}
 
     entity.setAttribute('gltf-model', model.url);
+    entity.setAttribute('animation-mixer', '');
 
     const div = document.querySelector('.instructions');
     div.innerText = model.info;
@@ -44,21 +44,10 @@ function renderPlaces(places) {
     places.forEach((place) => {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
+        let entity = document.createElement('a-entity');
 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-
-        setModel(models[modelIndex], model);
-
-        model.setAttribute('animation-mixer', '');
-
-        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('[gps-entity-place]');
-            modelIndex++;
-            var newIndex = modelIndex % models.length;
-            setModel(models[newIndex], entity);
-        });
-
-        scene.appendChild(model);
+        entity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        setModel(models[0], entity);
+        scene.appendChild(entity);
     });
 }
